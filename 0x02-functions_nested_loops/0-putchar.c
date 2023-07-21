aca4911407556b30df9319671bf4632f83a93065
Levi <levis2kk@gmail.com>
2023-07-21 05:15:24 -0700

    all tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints _putchar, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Print each character of "_putchar" using _putchar function */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	/* Print a new line using _putchar function */
+	_putchar('\n');
+
+	return (0);
+}
